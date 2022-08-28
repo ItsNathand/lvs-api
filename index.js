676ef0841cc2b0ea4fb8826d64f08f47ce596920
Nathan Demers <nathancodes@icloud.com>
2022-08-28 18:36:18 +0200

    setup completed

---
+++ index.js
@@ -0,0 +1,34 @@
+ import {
+    lvs
+} from './lib/lvs.js';
+
+//you can configure a cred.js file like this:
+/* let creds = {
+    "login": "",
+    "password":"",
+    "institut":".la-vie-scolaire.fr"
+} */
+
+export { creds }
+
+import {
+    creds
+}
+from './creds.js'
+
+(async () => {
+try {
+
+  let api = new lvs(creds);
+
+  let devoirs = await api.get('devoirs');
+  
+
+let result = await devoirs.export('json', {from:["10", "03", "2022"], to:["30", "03", "2022"]});
+console.log(result);
+//console.log(result.activites[0].activiteDocuments);
+} catch (error) {
+  console.log(error)
+} 
+
+})();
